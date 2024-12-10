d4b4695fd264ef771b53b34377eafd128084a56d
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-10 22:44:47 +0530

    this is my two hundred ninty four commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import About from "./components/About";
 import Labs from "./components/Lab";
 import NotFound from "./components/NotFound";
 import Support from "./components/Support";
+import {Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/support">Support</Link></li>
+          <li><Link to="/about"></Link>About</li>
+          <li><Link to="/labs">Labs</Link></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/support" element={<Support/>}></Route>
