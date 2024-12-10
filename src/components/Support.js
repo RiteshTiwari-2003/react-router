01e02f1ad3f0e62b6feccc827e2db737e3237e38
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-11 03:10:46 +0530

    this is my two hundred ninty six commit

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 const Support = () => {
+    const navigate=useNavigate();
+    function backHandler(){
+        navigate(-1);
+    }
+    function clickHandler(){
+        navigate("/labs");
+
+    }
   return (
     <div>
+        <div>
       this is dsupport page
     </div>
+    <button onClick={clickHandler}>Move to lab page</button>
+    <button onClick={backHandler}></button>
+    </div>
+    
   )
 }
 
